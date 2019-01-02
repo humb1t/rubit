8bea4c00617a8bb99f22348abf4ecb717b5215e5
Nikita <Nikita_Puzankov@epam.com>
2019-01-02 16:23:49 +0400

    Some base drafts about qubits and gates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, quantum world!");
+}
+
+enum StandardBasisVector{
+    Ground, Exited, 
+}
+
+struct Qubit{
+    state: StandardBasisVector,
+}
+
+impl Qubit {
+    fn is_active(&self)->bool{
+        match self.state {
+            StandardBasisVector::Exited => true,
+            _ => false,
+        }
+    }
+
+    fn activate(&mut self){
+        self.state = StandardBasisVector::Exited
+    }
+}
+
+struct CNOT {
+    control: Qubit,
+    target: Qubit,
+}
+
+trait Gate {
+    fn apply(&mut self); 
+}
+
+impl Gate for CNOT {
+    fn apply(&mut self) {
+       if self.control.is_active() { 
+           self.target.activate()
+       }
+    }
 }
